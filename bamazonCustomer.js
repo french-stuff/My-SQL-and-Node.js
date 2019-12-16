1a96dd81bde6df205a3b5c99af99a5f0b9d3e38f
Franchesca <legarreta.french@gmail.com>
2019-12-16 16:38:20 -0500

    created connection variable

---
+++ bamazonCustomer.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "Ca5aBlanca310",
+    database: "bamazon_db"
+
+});

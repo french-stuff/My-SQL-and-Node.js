7d3c3bfeac236edb20d861113279e75087634f11
French Stuff <legarreta.french@gmail.com>
2020-03-06 01:12:51 -0500

    added product result info

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,3 +11,30 @@ const connection = mysql.createConnection({
     database: "bamazon_db"
 
 });
+
+const query = connection.query(
+    "SELECT * FROM products",(err, res)=>{ 
+    
+    if (err) throw err;
+
+    for (let i = 0 ; i < res.length; i++){
+    console.log(
+    `
+    ========================
+    Item ID: ${res[i].item_id}
+    Product Name: ${res[i].product_name}
+    Department: ${res[i].department}
+    Price: $${res[i].price}
+    Quantity: ${res[i].stock_quantity}
+    ========================
+    `);
+
+}
+
+
+    connection.end();
+    })
+
+
+
+};
